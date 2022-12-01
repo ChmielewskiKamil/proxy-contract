280171aaed3a502a091c2a82a860b8a7d6da580e
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2022-12-01 17:56:06 +0100

    chore: add example contract

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,27 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Deployer {
+    function deployContractsAndSet() public returns (ERC20, ERC20) {
+        ERC20 contract1 = new ERC20("a", "b");
+        ERC20 contract2 = new ERC20("a2", "b2");
+
+        return (contract1, contract2);
+    }
+}
+
+contract EchidnaTest {
+    ERC20 a;
+    ERC20 b;
+
+    constructor() {
+        Deployer deployer = new Deployer();
+        (a, b) = deployer.deployContractsAndSet();
     }
 
-    function increment() public {
-        number++;
+    function echidna_da() public view returns (bool) {
+        return true;
     }
 }
